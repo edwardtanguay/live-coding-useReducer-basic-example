75ec1a8dc1602dfe7e521c2558ab457dcce7bd2e
Edward Tanguay (windows) <edward@tanguay.info>
2022-05-17 10:03:19 +0200

    plus/minus works

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
-import './App.scss'
+import { useReducer } from 'react';
+import './App.scss';
+
+const initialState = {
+    number: 0,
+    lastAction: ''
+};
+
+const reducer = (state, action) => {
+	switch (action.type) {
+		case 'down':
+			return {
+				number: state.number - 1,
+				lastAction: 'decrement'
+			};
+		case 'up':
+			return {
+				number: state.number + 1,
+				lastAction: 'increment'
+			};
+	}
+};
 
 function App() {
-  return (
-    <div className="App">
-		<h1>Info Site</h1>
-		<p>Welcome to this site.</p>
-    </div>
-  )
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    return (
+        <div className="App">
+            <h1>useReducer</h1>
+            <div>Number: {state.number}</div>
+			<div>Last action: {state.lastAction}</div>
+            <div className="buttonArea">
+                <button onClick={() => dispatch({type: 'down'})}>-</button>
+                <button onClick={() => dispatch({type: 'up'})}>+</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
